a70b813c6bfa1f16e31b42c10f6751d459066c98
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2023-05-27 00:59:07 +0300

    added high score tracking

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-
+import os
 pygame.init()
 
 #setting game window size (in pixels) (constant variables):
@@ -23,9 +23,19 @@ scroll = 0
 background_scroll = 0
 game_over = False
 score = 0
+fade_counter = 0
+
+if os.path.exists('score.txt'):
+    with open('score.txt', 'r') as file:
+        high_score = int(file.read())
+else:
+    high_score = 0
 
 #defining color scheme:
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PANEL_COLOR = (153, 217, 234)
+
 #Font
 font_small = pygame.font.SysFont('Lucida Sans', 20)
 font_big = pygame.font.SysFont('Lucida Sans', 24)
@@ -39,9 +49,17 @@ def draw_text(text, font, text_color, x, y):
 
 
 #loading game images:
-player_sprite = pygame.image.load('Desktop/PygamePlatformer/assets/cat.png').convert_alpha()
-platform_sprite = pygame.image.load('Desktop/PygamePlatformer/assets/platform.png').convert_alpha()
-bg_image = pygame.image.load("Desktop/PygamePlatformer/assets/background.jpg").convert_alpha()
+player_sprite = pygame.image.load('assets/cat.png').convert_alpha()
+platform_sprite = pygame.image.load('assets/platform.png').convert_alpha()
+bg_image = pygame.image.load("assets/background.jpg").convert_alpha()
+
+
+def draw_panel():
+    pygame.draw.rect(screen, PANEL_COLOR, (0,0, SCREEN_WIDTH, 30) )
+    pygame.draw.line(screen, WHITE, (0, 30), (SCREEN_WIDTH, 30), 2)
+    draw_text('YOUR SCORE: ' + str(score), font_small, WHITE, 0, 0)
+    
+
 
 # to draw infinite scrolling background
 def draw_background (background_scroll):
@@ -152,6 +170,7 @@ platform_group.add(platform)
 run = True
 while run:
     clock.tick(FPS)
+    
     if game_over == False: 
         scroll = player.move()
         
@@ -169,19 +188,22 @@ while run:
             platform_y = platform.rect.y - random.randint(80, 120)
             
             platform = Platform(platform_x, platform_y, platform_width)
-            platform_group.add(platform)
-        
-        
+            platform_group.add(platform)  
         #update platforms
         platform_group.update(scroll)
         
+        #updating the score
+        if scroll > 0:
+            score += scroll
+        #draw line at previous highest score
+        pygame.draw.liene(screen, WHITE, (0, score - high_score + SCROLL_THRESHOLD), (SCREEN_WIDTH, score - high_score + SCROLL_THRESHOLD), 3 )
+        draw_text('HIGHEST SCORE', font_small, WHITE, SCREEN_WIDTH - 130, score - high_score + SCROLL_THRESHOLD)
+        
         #draw sprites:
         platform_group.draw(screen)
-        player.draw()
-        
-        #draw temporary scroll threshold
-        #pygame.draw.line(screen, WHITE, (0, SCROLL_THRESHOLD), (SCREEN_WIDTH, SCROLL_THRESHOLD))
-        
+        player.draw()        
+        #draw panel
+        draw_panel()
         
         #check for game over
         if player.rectangle.top > SCREEN_HEIGHT:
@@ -190,29 +212,48 @@ while run:
     
     else:
         
-        draw_text('GAME OVER!', font_big, WHITE, 130, 200)
-        draw_text('SCORE: ' + str(score), font_big, WHITE, 130, 250)
-        draw_text('PRESS SPACE TO PLAY AGAIN', font_big, WHITE, 40, 300)
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE]:
-            #reset variables
-            game_over = False
-            score = 0
-            scroll = 0
+        if fade_counter < SCREEN_WIDTH:
+            fade_counter += 5 
+            for y in range(0, 6, 2):
+                pygame.draw.rect(screen, BLACK, (0, y * 100, fade_counter, SCREEN_HEIGHT / 6 ))
+                pygame.draw.rect(screen, BLACK, (SCREEN_WIDTH - fade_counter, (y+1)*100, SCREEN_WIDTH, SCREEN_HEIGHT/ 6 ))
+        else:    
+            draw_text('GAME OVER!', font_big, WHITE, 130, 200)
+            draw_text('SCORE: ' + str(score), font_big, WHITE, 130, 250)
+            draw_text('PRESS SPACE TO PLAY AGAIN', font_big, WHITE, 40, 300)
+            #update highest score
+            if score > high_score:
+                high_score = score
+                with open('score.txt', 'w') as file:
+                    file.write(str(high_score))
             
-            #reposition player
-            player.rectangle.center = (SCREEN_WIDTH // 2 , SCREEN_HEIGHT - 150 )
-            #reset platforms
-            platform_group.empty()
-            #create starting platform
-            platform = Platform(SCREEN_WIDTH // 2 - 50 , SCREEN_HEIGHT - 50, 100)
-            platform_group.add(platform)
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE]:
+                #reset variables
+                game_over = False
+                score = 0
+                scroll = 0
+                fade_counter = 0
+                
+                #reposition player
+                player.rectangle.center = (SCREEN_WIDTH // 2 , SCREEN_HEIGHT - 150 )
+                #reset platforms
+                platform_group.empty()
+                #create starting platform
+                platform = Platform(SCREEN_WIDTH // 2 - 50 , SCREEN_HEIGHT - 50, 100)
+                platform_group.add(platform)
 
-            
+                
           
     #event handler - click 'X' to close game window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            #update highest score
+            if score > high_score:
+                high_score = score
+                with open('score.txt', 'w') as file:
+                    file.write(str(high_score))
+            
             run = False
     
     #update display window
